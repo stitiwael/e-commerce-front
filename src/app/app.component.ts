0d35f3a15a22c1df0256d00ff4c5a39c291b20a6
waell st <waelstiti7@gmail.com>
2021-10-11 13:17:35 +0100

    j'ai ajouté un template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Inject, Injectable, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
+import { DOCUMENT } from '@angular/common'; 
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'e-commerce-frontend';
+  constructor( @Inject(DOCUMENT) private document: Document) {}
+  // Open and close sidebar
+therichpost_open() {
+  document.getElementById("mySidebar")!.style.display = "block";
+  document.getElementById("myOverlay")!.style.display = "block";
 }
+ 
+therichpost_close() {
+  document.getElementById("mySidebar")!.style.display = "none";
+  document.getElementById("myOverlay")!.style.display = "none";
+}
+}
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,25 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TestComponent } from './test/test.component';
 import { DataComponent } from './data/data.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material/material.module';
+import { NavigationComponent } from './navigation/navigation.component';
+import { FooterComponent } from './footer/footer.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     TestComponent,
-    DataComponent
+    DataComponent,
+    NavigationComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
